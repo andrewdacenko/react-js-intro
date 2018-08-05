91c8cd955ac6e46881642af44ef78452488f7042
Andrew Dacenko <a.s.dvornik@gmail.com>
2018-08-05 22:45:42 +0100

    state-updates

--- src/index.js
+++ src/index.js
@@ -1,14 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
-class HelloMessage extends React.Component {
+class Counter extends React.Component {
   render() {
-    return <div>{'Hello, ' + this.props.message + '!'}</div>;
+    if (this.props.count === 0) {
+      return <p>Start clicking</p>;
+    }
+
+    return (
+      <p>
+        <span>You have clicked</span>
+        {' '}
+        <span className="label">{this.props.count}</span>
+        {' '}
+        <span>times</span>
+      </p>
+    );
+  }
+}
+
+class Clicker extends React.Component {
+  state = {
+    count: 0,
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <button className="button" onClick={() => {
+          this.setState({
+            count: this.state.count + 1,
+          });
+        }}>
+          Increment
+        </button>
+        <Counter count={this.state.count}/>
+      </div>
+    );
   }
 }
 
 ReactDOM.render(
-  // TODO: change "World" to your name
-  <HelloMessage message="World"/>,
+  <Clicker />,
   document.getElementById('root')
 );
